2c3f1e1bf1495b58176fe3407f69e3a524fdda3a
David Larsen <davidplarsen@gmail.com>
2020-09-22 13:27:59 -0500

    Part 2 - get both numbers in a single prompt

---
+++ Calculatron/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculatron
+{
+    public class Calculator
+    {
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int? Division(int num1, int num2)
+        {
+            try
+            {
+                return num1 / num2;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+                return null;
+            }
+        }
+    }
+}
